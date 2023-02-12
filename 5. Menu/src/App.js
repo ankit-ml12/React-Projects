816bd4bd41158d472e27e3651ea18dbdfd80ba2f
ankit patidar <patiankit6555@gmail.com>
2023-02-12 16:26:41 +0530

    menu completed

--- 5. Menu/src/App.js
+++ 5. Menu/src/App.js
@@ -1,10 +1,35 @@
-import React, { useState } from 'react';
-import Menu from './Menu';
-import Categories from './Categories';
-import items from './data';
+import React, { useState } from 'react'
+import Menu from './Menu'
+import items from './data'
+import Categories from './Categories'
+
+const allCategories = ['all', ...new Set(items.map((item) => item.category))]
 
 function App() {
-  return <h2>menu project setup</h2>;
+  console.log(items)
+  const [menuItems, setmenuItems] = useState(items)
+  const [categories, setCategories] = useState(allCategories)
+
+  const filterItem = (category) => {
+    if (category === 'all') {
+      setmenuItems(items)
+      return
+    }
+    const newitems = items.filter((item) => item.category == category)
+    setmenuItems(newitems)
+  }
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>Our Menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories categories={categories} filterItem={filterItem} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  )
 }
 
-export default App;
+export default App
