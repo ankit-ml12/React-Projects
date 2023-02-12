24852c80e2e58e304576f4653a492a79014b0aeb
ankit patidar <patiankit6555@gmail.com>
2023-02-12 19:52:41 +0530

    tab complete

--- 6. Tabs/src/App.js
+++ 6. Tabs/src/App.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { FaAngleDoubleRight } from 'react-icons/fa'
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
+
 const url = 'https://course-api.com/react-tabs-project'
 function App() {
   const [loading, setLoading] = useState(true)
@@ -17,6 +16,7 @@ function App() {
   useEffect(() => {
     fetchJobs()
   }, [])
+
   if (loading) {
     return (
       <section className="section loading">
@@ -28,11 +28,10 @@ function App() {
   return (
     <section className="section">
       <div className="title">
-        <h2>experience</h2>
+        <h2>Experience</h2>
         <div className="underline"></div>
       </div>
       <div className="jobs-center">
-        {/* btn container */}
         <div className="btn-container">
           {jobs.map((item, index) => {
             return (
@@ -46,7 +45,6 @@ function App() {
             )
           })}
         </div>
-        {/* job info */}
         <article className="job-info">
           <h3>{title}</h3>
           <h4>{company}</h4>
@@ -54,16 +52,13 @@ function App() {
           {duties.map((duty, index) => {
             return (
               <div key={index} className="job-desc">
-                <FaAngleDoubleRight className="job-icon"></FaAngleDoubleRight>
+                <FaAngleDoubleRight className="job-icon" />
                 <p>{duty}</p>
               </div>
             )
           })}
         </article>
       </div>
-      <button type="button" className="btn">
-        more info
-      </button>
     </section>
   )
 }
