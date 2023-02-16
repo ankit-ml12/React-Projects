72784f09aa106a9890ed33741f8b5d3d269749e2
ankit patidar <patiankit6555@gmail.com>
2023-02-16 17:30:57 +0530

    navbar completed

--- 11 Navbar/src/Navbar.js
+++ 11 Navbar/src/Navbar.js
@@ -4,7 +4,52 @@ import { links, social } from './data'
 import logo from './logo.svg'
 
 const Navbar = () => {
-  return <h4>navbar</h4>
+  const [showlink, setShowlink] = useState(false)
+  const linksContainerRefs = useRef(null)
+  const linksRef = useRef(null)
+
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height
+    console.log(linksHeight)
+    if (showlink) {
+      linksRef.current.style.height = `${linksHeight}`
+    } else {
+      linksRef.current.style.height = `0px`
+    }
+  }, [showlink])
+  return (
+    <div className="nav-center">
+      <div className="nav-header">
+        <img src={logo} alt="logo" />
+        <button className="nav-toggle" onClick={() => setShowlink(!showlink)}>
+          <FaBars />
+        </button>
+      </div>
+
+      <div className="link-container" ref={linksContainerRefs}>
+        <ul className="links" ref={linksRef}>
+          {links.map((link) => {
+            const { id, url, text } = link
+            return (
+              <li key={id}>
+                <a href={url}>{text}</a>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+      <ul className="social-icons">
+        {social.map((soci) => {
+          const { id, url, icon } = soci
+          return (
+            <li key={id}>
+              <a href={url}>{icon}</a>
+            </li>
+          )
+        })}
+      </ul>
+    </div>
+  )
 }
 
 export default Navbar
