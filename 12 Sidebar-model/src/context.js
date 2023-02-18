9adaa90e780b002c5b18d12df472c2be8607c17b
ankit patidar <patiankit6555@gmail.com>
2023-02-18 15:06:08 +0530

    sidebar complete

--- 12 Sidebar-model/src/context.js
+++ 12 Sidebar-model/src/context.js
@@ -1,1 +1,42 @@
 import React, { useState, useContext } from 'react'
+
+const AppContext = React.createContext()
+
+const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false)
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  const openSidebar = () => {
+    setIsSidebarOpen(true)
+  }
+  const closeSidebar = () => {
+    setIsSidebarOpen(false)
+  }
+  const openModal = () => {
+    setIsModalOpen(true)
+  }
+  const closeModal = () => {
+    setIsModalOpen(false)
+  }
+  return (
+    <AppContext.Provider
+      value={{
+        openSidebar,
+        closeSidebar,
+        openModal,
+        closeModal,
+        isSidebarOpen,
+        isModalOpen,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  )
+}
+
+//custom hook for avaoid again again importing of Appcontext and AppConotext
+
+export const useGlobalContext = () => {
+  return useContext(AppContext)
+}
+export { AppContext, AppProvider }
