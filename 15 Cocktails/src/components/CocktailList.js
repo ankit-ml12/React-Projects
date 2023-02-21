82143173c493f9b8b1979807a7cbe5fffb10f2b0
ankit patidar <patiankit6555@gmail.com>
2023-02-21 21:18:09 +0530

    coctail midway

--- 15 Cocktails/src/components/CocktailList.js
+++ 15 Cocktails/src/components/CocktailList.js
@@ -4,10 +4,23 @@ import Loading from './Loading'
 import { useGlobalContext } from '../context'
 
 const CocktailList = () => {
+  const { cocktails, loading } = useGlobalContext()
+  console.log('load', loading)
+  if (loading) return <Loading />
+  if (cocktails.length < 1) {
+    return (
+      <h2 className="section-title">No Cocktails match your search criteria</h2>
+    )
+  }
   return (
-    <div>
-      <h2>cocktail list component</h2>
-    </div>
+    <section className="section">
+      <h2 className="section-title">cocktails</h2>
+      <div className="cocktails-center">
+        {cocktails.map((item) => {
+          return <Cocktail key={item.id} {...item} />
+        })}
+      </div>
+    </section>
   )
 }
 
--- 15 Cocktails/src/pages/About.js
+++ 15 Cocktails/src/pages/About.js
@@ -2,9 +2,16 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className="section about-section">
+      <h1 className="section-title">About Us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. At, eos vitae,
+        distinctio nam dolore laborum quo quae voluptatem maiores dicta
+        praesentium doloribus libero repudiandae architecto sint quidem quia
+        dolor id, amet illo vel assumenda ex mollitia voluptates? Aliquam, neque
+        harum!
+      </p>
+    </section>
   )
 }
 
