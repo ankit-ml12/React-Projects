01fbae4f2f2986fa7caf70a9cb85b72e5604d767
ankit patidar <patiankit6555@gmail.com>
2023-02-24 16:17:27 +0530

    stockPhotos Complete

--- 19 Stock Photo/src/App.js
+++ 19 Stock Photo/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { FaSearch } from 'react-icons/fa'
 import Photo from './Photo'
 const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
@@ -8,9 +8,10 @@ const searchUrl = `https://api.unsplash.com/search/photos/`
 function App() {
   const [loading, setLoading] = useState(false)
   const [photos, setPhotos] = useState([])
-  const [page, setPage] = useState(0)
+  const [page, setPage] = useState(1)
   const [query, setQuery] = useState('')
-
+  const mounted = useRef(false)
+  const [newImages, setNewImages] = useState(false)
   const fetchImages = async () => {
     setLoading(true)
     let url
@@ -35,9 +36,10 @@ function App() {
           return [...oldPhotos, ...data]
         }
       })
-
+      setNewImages(false)
       setLoading(false)
     } catch (error) {
+      setNewImages(false)
       setLoading(false)
       console.log('error :', error)
     }
@@ -46,24 +48,45 @@ function App() {
     fetchImages()
   }, [page])
 
+  // useEffect(() => {
+  //   const event = window.addEventListener('scroll', () => {
+  //     if (
+  //       !loading &&
+  //       window.innerHeight + window.scrollY >= document.body.scrollHeight - 2
+  //     ) {
+  //       setPage((oldPage) => {
+  //         return 1 + oldPage
+  //       })
+  //     }
+  //   })
+  //   return () => window.removeEventListener('scroll', event)
+  // }, [])
   useEffect(() => {
-    const event = window.addEventListener('scroll', () => {
-      if (
-        !loading &&
-        window.innerHeight + window.scrollY >= document.body.scrollHeight - 2
-      ) {
-        setPage((oldPage) => {
-          return 1 + oldPage
-        })
-      }
-    })
-    return () => window.removeEventListener('scroll', event)
-  }, [])
+    if (!mounted.current) {
+      mounted.current = true
+      return
+    }
+    if (!newImages) return
+    if (loading) return
+    setPage((oldPage) => oldPage + 1)
+  }, [newImages])
 
+  const event = () => {
+    if (window.innerHeight + window.scrollY >= document.body.scrollHeight - 2) {
+      setNewImages(true)
+    }
+  }
+  useEffect(() => {
+    window.addEventListener('scroll', event)
+    return () => window.removeEventListener('scroll', event)
+  })
   const handleSubmit = (e) => {
     e.preventDefault()
+    if (!query) return
+    if (page === 1) {
+      fetchImages()
+    }
     setPage(1)
-    fetchImages()
   }
   return (
     <main>
