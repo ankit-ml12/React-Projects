5ad0e582be6a14e7348709236f55449b343de48e
ankit patidar <patiankit6555@gmail.com>
2023-02-26 12:02:08 +0530

    hacker news complete

--- 22 Hackers-News/src/reducer.js
+++ 22 Hackers-News/src/reducer.js
@@ -6,5 +6,42 @@ import {
   HANDLE_SEARCH,
 } from './actions'
 
-const reducer = () => {}
+const reducer = (state, action) => {
+  switch (action.type) {
+    case SET_LOADING:
+      return { ...state, isLoading: true }
+    case SET_STORIES:
+      return {
+        ...state,
+        isLoading: false,
+        hits: action.payload.hits,
+        nbPages: action.payload.nbPages,
+      }
+    case REMOVE_STORY:
+      return {
+        ...state,
+        hits: state.hits.filter((story) => story.objectID !== action.payload),
+      }
+    case HANDLE_SEARCH:
+      return {
+        ...state,
+        query: action.payload,
+        page: 0,
+      }
+    case HANDLE_PAGE:
+      let newPage
+      if (action.payload == 'inc') {
+        newPage = (1 + state.page) % state.nbPages
+      } else if (action.payload == 'dec') {
+        newPage = (state.page + state.nbPages - 1) % state.nbPages
+      }
+      return {
+        ...state,
+        page: newPage,
+      }
+
+    default:
+      throw new Error(`no mathcing " ${action.type} " action type`)
+  }
+}
 export default reducer
