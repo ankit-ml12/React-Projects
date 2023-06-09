6b2ff848239f55ebc1ec05de16f6b37035a21821
ankit patidar <patiankit6555@gmail.com>
2023-06-10 05:14:27 +0530

    get task

--- 24-react-query/starter/src/Items.jsx
+++ 24-react-query/starter/src/Items.jsx
@@ -1,11 +1,29 @@
-import SingleItem from './SingleItem';
-const Items = ({ items }) => {
+import SingleItem from './SingleItem'
+import { useQuery } from '@tanstack/react-query'
+import customFetch from './utils'
+const Items = () => {
+  const { isLoading, data } = useQuery({
+    queryKey: ['tasks'],
+    queryFn: async () => {
+      const { data } = await customFetch.get('/')
+      return data
+    },
+  })
+  // const { isLoading, data } = useQuery({
+  //   queryKey: ['tasks'],
+  //   queryFn: () => customFetch.get('/'),
+  // })
+
+  if (isLoading) {
+    return <h2>Loading ...</h2>
+  }
+
   return (
-    <div className='items'>
-      {items.map((item) => {
-        return <SingleItem key={item.id} item={item} />;
+    <div className="items">
+      {data.taskList.map((item) => {
+        return <SingleItem key={item.id} item={item} />
       })}
     </div>
-  );
-};
-export default Items;
+  )
+}
+export default Items
